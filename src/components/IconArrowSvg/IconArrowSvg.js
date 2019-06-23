57c2dbae73747f61901cdded7c34a41aeb95d8b7
My420 <my420V@gmail.com>
2019-06-23 18:22:22 +0300

    add IconArrowSvg component with test

---
+++ src/components/IconArrowSvg/IconArrowSvg.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './IconArrowSvg.module.scss';
+
+const IconArrowSvg = ({ direction, containerClass, pathClass }) => {
+  const containerClassName = `${styles.container} ${containerClass} ${
+    styles[direction]
+  }`;
+
+  return (
+    <div className={containerClassName}>
+      <svg viewBox="0 0 512 512" preserveAspectRatio="xMinYMin meet">
+        <path
+          className={pathClass}
+          d="M352 128.4L319.7 96 160 256l159.7 160 32.3-32.4L224.7 256z"
+        />
+      </svg>
+    </div>
+  );
+};
+
+IconArrowSvg.propTypes = {
+  direction: PropTypes.oneOf(['left', 'right', 'top', 'bottom']),
+  containerClass: PropTypes.string,
+  pathClass: PropTypes.string
+};
+
+IconArrowSvg.defaultProps = {
+  direction: 'left',
+  containerClass: styles.svgContainer,
+  pathClass: styles.path
+};
+
+export default IconArrowSvg;
---
+++ src/components/IconArrowSvg/IconArrowSvg.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import IconArrowSvg from './IconArrowSvg';
+
+describe('Test <IconArrowSvg /> component', () => {
+  describe('Test <IconArrowSvg /> without props', () => {
+    const wrapper = shallow(<IconArrowSvg />);
+
+    test('should correct render component without props', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should  render div with default className', () => {
+      expect(wrapper.find('div').prop('className')).toBe(
+        'container svgContainer left'
+      );
+    });
+
+    test("should  render svg'", () => {
+      expect(wrapper.find('svg')).toHaveLength(1);
+    });
+
+    test('svg should  have path with default className', () => {
+      expect(wrapper.find('path').prop('className')).toBe('path');
+    });
+  });
+
+  describe('Test <IconArrowSvg /> with props', () => {
+    const wrapper = shallow(
+      <IconArrowSvg
+        direction="top"
+        containerClass="testContainer"
+        pathClass="testPath"
+      />
+    );
+
+    test('should correct render component with props', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should  correct set className at div ', () => {
+      expect(wrapper.find('div').prop('className')).toBe(
+        'container testContainer top'
+      );
+    });
+
+    test('should  correct set className at path', () => {
+      expect(wrapper.find('path').prop('className')).toBe('testPath');
+    });
+  });
+});
