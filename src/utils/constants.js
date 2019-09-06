6ac8a75b86dc766a07e8badfb78f7a42bbb92747
My420 <my420V@gmail.com>
2019-09-06 16:45:53 +0300

    add LocalStorage class with test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -44,6 +44,10 @@ export const SOCIAL_LINK = {
 
 export const AUTHOR = 'Klinovitsky Aleksey';
 
+export const APP_LOCALSTORAGE_KEY = '__SOME_BEER_APP__';
+
+export const FAVORITE = 'FAVORITE';
+
 export const POPULAR_BEER__GET_REQUEST = 'POPULAR_BEER__GET_REQUEST';
 export const POPULAR_BEER__GET_SUCCESS = 'POPULAR_BEER__GET_SUCCESS';
 export const POPULAR_BEER__GET_FAILURE = 'POPULAR_BEER__GET_FAILURE';
