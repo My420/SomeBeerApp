adab3876834adcbbe72414a395b0ae520bda05aa
My420 <my420V@gmail.com>
2019-10-23 20:56:29 +0300

    add CartPage component

--- src/components/CartCard/CartCard.js
+++ src/components/CartCard/CartCard.js
@@ -3,15 +3,35 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './CartCard.module.scss';
 import Button from '../Button/Button';
+import IconTrashCan from '../IconTrashCan/IconTrashCan';
 
-/* changeAmount, deleteCard */
-
-const CartCard = ({ data }) => {
+const CartCard = ({ data, changeAmount, deleteCard }) => {
   // eslint-disable-next-line no-console
   console.log('render ==================== CartCard');
 
-  const { amount, item } = data;
+  let { amount } = data;
+  const { item } = data;
   const { image_url: url, name, id } = item;
+
+  const onAmountChange = evt => {
+    const { action } = evt.target.dataset;
+
+    if (action === 'increase') {
+      amount += 1;
+      changeAmount(id, amount);
+    } else if (action === 'decrease') {
+      amount -= 1;
+      if (amount === 0) {
+        deleteCard(id);
+      } else {
+        changeAmount(id, amount);
+      }
+    }
+  };
+
+  const onDeleteClick = () => {
+    deleteCard(id);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -25,17 +45,29 @@ const CartCard = ({ data }) => {
       </div>
       <div className={styles.controlsWrapper}>
         <div className={styles.amountWrapper}>
-          <Button className={styles.decrease}>
+          <Button
+            className={styles.decrease}
+            onClick={onAmountChange}
+            data-action="decrease"
+          >
             <span className="visually-hidden">decrease</span>-
           </Button>
           <span className={styles.amount}>{amount}</span>
-          <Button className={styles.increase}>
+          <Button
+            className={styles.increase}
+            onClick={onAmountChange}
+            data-action="increase"
+          >
             <span className="visually-hidden">increase</span>+
           </Button>
         </div>
 
-        <Button className={styles.delete}>
-          <span className="visually-hidden">delete</span>delete
+        <Button className={styles.delete} onClick={onDeleteClick}>
+          <span className="visually-hidden">delete</span>
+          <IconTrashCan
+            containerClass={styles.deleteContainer}
+            pathClass={styles.deletePath}
+          />
         </Button>
       </div>
     </div>
@@ -44,9 +76,9 @@ const CartCard = ({ data }) => {
 
 CartCard.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  data: PropTypes.object.isRequired
-  // changeAmount: PropTypes.func.isRequired,
-  // deleteCard: PropTypes.func.isRequired
+  data: PropTypes.object.isRequired,
+  changeAmount: PropTypes.func.isRequired,
+  deleteCard: PropTypes.func.isRequired
 };
 
 export default CartCard;
---
+++ src/components/IconTrashCan/IconTrashCan.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './IconTrashCan.module.scss';
+
+const IconTrashCan = ({ pathClass, wrapperClass }) => {
+  return (
+    <div className={wrapperClass}>
+      <svg width="100%" height="100%" viewBox="0 0 24 24">
+        <path
+          className={pathClass}
+          d="M9 19c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5-17v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712zm-3 4v16h-14v-16h-2v18h18v-18h-2z"
+        />
+      </svg>
+    </div>
+  );
+};
+
+IconTrashCan.propTypes = {
+  pathClass: PropTypes.string,
+  wrapperClass: PropTypes.string
+};
+
+IconTrashCan.defaultProps = {
+  pathClass: styles.path,
+  wrapperClass: styles.container
+};
+
+export default IconTrashCan;
